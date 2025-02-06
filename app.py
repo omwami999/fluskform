486f83499bfe48d27753a1803ad09d263342fd1c
Oliver Wanjala <43819586+omwami999@users.noreply.github.com>
2025-02-06 05:01:57 +0000

    staged

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ from flask import Flask, render_template, request, url_for, flash, redirect
 from flask import Flask, render_template
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'c83d87b8db96b34d7b4a758ba6585b2982014f5ded02d71b'
 
 messages = [{'title': 'Message One',
              'content': 'Message One Content'},
@@ -14,8 +15,19 @@ messages = [{'title': 'Message One',
 @app.route('/')
 def index():
     return render_template('index.html', messages=messages)
-# ...
 
 @app.route('/create/', methods=('GET', 'POST'))
 def create():
+    if request.method == 'POST':
+        title = request.form['title']
+        content = request.form['content']
+
+        if not title:
+            flash('Title is required!')
+        elif not content:
+            flash('Content is required!')
+        else:
+            messages.append({'title': title, 'content': content})
+            return redirect(url_for('index'))
+
     return render_template('create.html')
